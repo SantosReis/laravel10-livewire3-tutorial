1066a34bf87d6877284bdf4b71c6338da44a127b
Marcus Reis <mvsantosreis@gmail.com>
2025-01-25 19:11:20 +0000

    mounting properties

---
+++ resources/views/livewire/tasks.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <h1>Tasks</h1>
+
+    <input type="text" wire:model="task">
+
+    <button wire:click="add" class="bg-indigo-500 p-2">Add</button>
+
+    <ul>
+        @foreach ($tasks as $task)
+            <li>{{ $task }}</li>
+        @endforeach
+    </ul>
+</div>
